d00441e9ff9cf4d7ed57c1004f37624070b0e9c6
RegisSalesRA <regismsales@gmail.com>
2022-03-03 05:04:56 -0300

    Created endpoints post

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from core.serializers import *
+from core.models import *
 
-# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
