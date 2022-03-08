94a112e88eed8ed01e7212689b4d23b7db4c97ae
RegisSalesRA <regismsales@gmail.com>
2022-03-08 09:50:29 -0300

    BlackList jwt

--- core/v1/urls_v1.py
+++ core/v1/urls_v1.py
@@ -1,9 +1,26 @@
 from django.urls import path
 from core.v1.views import PostListView, PostDetailView, CategoryDetailView,CategoryListView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Django React API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('post/',PostListView.as_view()),
     path('post/<int:pk>',PostDetailView.as_view()),
     path('category/',CategoryListView.as_view()),
     path('category/<int:pk>',CategoryDetailView.as_view()),    
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   
 ]
--- core/v1/views.py
+++ core/v1/views.py
@@ -1,25 +1,25 @@
-from rest_framework import generics, filters
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from core.v1.serializers import CategoriaSerializer, PostSerializer
 from core.models import Category, Post
 
 
 class PostListView(generics.ListCreateAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class CategoryListView(generics.ListCreateAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     queryset = Category.objects.all()
     serializer_class = CategoriaSerializer
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     queryset = Category.objects.all()
     serializer_class = CategoriaSerializer
