7f92c0269f06f74d7baed4a779ef370ca3b334cd
RegisSalesRA <regismsales@gmail.com>
2022-03-26 14:46:46 -0300

    Refactory frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,11 @@
 import { GlobalStyle } from './styles/global';
-import { Dashboard } from './components/Dashboard';
-import {Header} from './components/Header/header';
-import { useState } from 'react'
-import Modal from 'react-modal';
-import { NewTransactionModal } from './components/Modals';
 
-Modal.setAppElement('#root');
 
 function App() {
 
-  const [isNewTransactionModalOpen, setisNewTransactionModalOpen] = useState(false);
-
-  function handleOpenNewTransactionModal(){
-      setisNewTransactionModalOpen(true);
-  }
-
-  function handleCloseNewTransactionModal(){
-      setisNewTransactionModalOpen(false);
-  }
-
   return (
     <>
-      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-      <Dashboard />
-      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}></NewTransactionModal>
       <GlobalStyle />
-
     </>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,51 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createServer, Model} from 'miragejs';
 import App from './App';
 
-createServer({
-  
-  models: {
-    transaction: Model,
-  },
-
-  seeds(server){
-    server.db.loadData({
-      transactions:[
-        {
-        id:1,
-        title:"Titulo 1",
-        type: "deposit",
-        category: 'Dev',
-        amount: 25,
-        createdAt: new Date('2012-02-02 09:00:00'),
-      },
-      {
-        id:2,
-        title:"Titulo 2",
-        type: "withdraw",
-        category: 'Dev 2',
-        amount: 22,
-        createdAt: new Date('2012-02-02 09:00:00'),
-      },
-    ],
-    })
-  },
-
-  routes(){
-    this.namespace = 'api';
-    
-    this.get('/transactions', () => {
-        return this.schema.all('transaction');
-    })
-
-    this.post('/transactions', (schema, request) =>{
-       const data = JSON.parse(request.requestBody)
-       return schema.create('transaction', data);
-    })
-    
-  }
-})
 
 ReactDOM.render(
   <React.StrictMode>
