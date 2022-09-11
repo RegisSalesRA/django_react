923e1d21e827dc8c9e2d1073bbd78d4c03e5ae15
RegisSalesRA <regismsales@gmail.com>
2022-09-10 21:46:21 -0300

    Creating components Footer,Header,Product and Screen Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
- 
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 function App() {
   return (
- <div>
-  My App
- </div>
+    <div>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
