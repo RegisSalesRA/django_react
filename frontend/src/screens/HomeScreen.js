36a474909339adb391b0fb604a1b01dc8c5009ea
RegisSalesRA <regismsales@gmail.com>
2022-09-12 22:22:24 -0300

    Card and routes implemented

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -11,7 +11,6 @@ function HomeScreen() {
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-
                         <Product product={product} />
                     </Col>
                 ))}
