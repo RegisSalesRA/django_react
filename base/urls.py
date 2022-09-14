61e2ee0dca02829a81be474236f64e6f9c278643
RegisSalesRA <regismsales@gmail.com>
2022-09-14 19:08:59 -0300

    jwt token implemented

--- base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from base import views
-
-urlpatterns = [
-    path('', views.getRoutes, name="routes"),
-    path('products/', views.getProducts, name="routes"),
-    path('products/<str:pk>/', views.getProduct, name="routes"),
-]
